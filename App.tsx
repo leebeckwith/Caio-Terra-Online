4b86d6402dbe19f903ed436dab38346a59b53fd6
aguilanegra <mateo.nares@gmail.com>
2023-07-21 18:00:49 -0700

    restructured app to use navigation, and added video listing page

--- App.tsx
+++ App.tsx
@@ -1,118 +1,25 @@
-/**
- * Caio Terra Online
- * Author: Mateo Nares
- *
- * @format
- */
-
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import {
-  Alert,
-  Button,
-  ImageBackground,
-  SafeAreaView,
-  StyleSheet,
-  TextInput,
-  View,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
+import VideoListingScreen from './VideoListingScreen';
+
+const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
-  // Data for login
-  const [log, setUsername] = useState('');
-  const [pwd, setPassword] = useState('');
-
-  // use API to validate username and password
-  const handleLogin = async () => {
-    if (!log || !pwd) {
-      Alert.alert('Invalid Login', 'Please enter a username and password.');
-      return;
-    }
-
-    try {
-      const formData = new FormData();
-      formData.append('log', log);
-      formData.append('pwd', pwd);
-      formData.append('lwa', '1');
-      formData.append('login-with-ajax', 'login');
-
-      const response = await fetch(
-        'https://caioterra.com/wp-admin/admin-ajax.php',
-        {
-          method: 'POST',
-          body: formData,
-        },
-      );
-
-      const responseData = await response.json();
-      console.log(responseData);
-
-      if (response.ok) {
-        if (responseData.result === false) {
-          Alert.alert('Error', responseData.error);
-        } else {
-          Alert.alert('Login Successful', 'Welcome!');
-        }
-      }
-    } catch (error) {
-      Alert.alert('Error', 'There was an error logging in.');
-    }
-  };
-
-  // show splash screen and hide when app is loaded
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
   return (
-    <SafeAreaView>
-      <ImageBackground
-        source={require('./assets/images/caio-terra-app.jpg')}
-        style={styles.background}>
-        <View style={styles.container}>
-          <TextInput
-            autoCapitalize="none"
-            placeholder="Username"
-            style={styles.input}
-            onChangeText={text => setUsername(text)}
-            placeholderTextColor={'rgba(255, 255, 255, 0.5)'}
-            value={log}
-          />
-          <TextInput
-            autoCapitalize="none"
-            placeholder="Password"
-            secureTextEntry
-            style={styles.input}
-            onChangeText={text => setPassword(text)}
-            placeholderTextColor={'rgba(255, 255, 255, 0.5)'}
-            value={pwd}
-          />
-          <Button title="Login" onPress={handleLogin} />
-        </View>
-      </ImageBackground>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Videos" component={VideoListingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  background: {
-    height: '100%',
-    width: '100%',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    color: '#ffffff',
-    width: '80%',
-    marginBottom: 20,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-  },
-});
 
 export default App;
